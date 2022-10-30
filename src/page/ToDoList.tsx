2c2bcc2ffa6212f2be5f9b01abf63ddea7a8c6f6
BobbyKJH <rlatnguscjsw@gmail.com>
2022-10-30 17:40:15 +0900

    Feat: Recoil Study

--- src/page/ToDoList.tsx
+++ src/page/ToDoList.tsx
@@ -1,77 +1,65 @@
 import { useForm } from "react-hook-form";
+import {
+  atom,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 
 interface IForm {
-  email: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string;
 }
 
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
 const ToDoList = () => {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-    setError,
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
+    setValue,
+  } = useForm<IForm>({});
 
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError("password1", { message: "패스워드가 일치하지 않습니다." });
-      console.log(watch());
-    }
-    setError("extraError", { message: "server down" });
+  const onSubmit = ({ toDo }: IForm) => {
+    setToDos((oldTodo) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...toDos,
+    ]);
+    setValue("toDo", "");
   };
 
-  console.log(errors);
-
+  console.log(toDos);
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          style={{ display: "flex" }}
-          {...register("email", {
-            required: "필수 입력",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "네이버 이메일로만 가입 가능합니다",
-            },
+          {...register("toDo", {
+            required: "Please Write a Todo",
           })}
           type="text"
-          placeholder="email"
+          placeholder="To Do List  "
         />
-
-        <input
-          style={{ display: "flex" }}
-          {...register("password", {
-            required: true,
-            minLength: 5,
-          })}
-          type="text"
-          placeholder="password"
-        />
-
-        <input
-          style={{ display: "flex" }}
-          {...register("password1", {
-            required: true,
-            validate: (value) => value.includes("kim"),
-          })}
-          type="text"
-          placeholder="password 확인"
-        />
-
-        <span>{errors.email?.message}</span>
-        <span>{errors.password1?.message}</span>
-
         <button>Add</button>
-        <span>{errors.extraError?.message}</span>
       </form>
+
+      <span>{errors?.toDo?.message}</span>
+
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 };
